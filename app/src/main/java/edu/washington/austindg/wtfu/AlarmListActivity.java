129d95ce6fc9e58776d6a853ee92c904c5282b13
austindg <austindg@uw.edu>
2015-02-27 16:51:56 -0800

    Added Alarm class

--- app/src/main/java/edu/washington/austindg/wtfu/AlarmListActivity.java
+++ app/src/main/java/edu/washington/austindg/wtfu/AlarmListActivity.java
@@ -1,9 +1,10 @@
 package edu.washington.austindg.wtfu;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 
 
 public class AlarmListActivity extends ActionBarActivity {
@@ -12,8 +13,21 @@ public class AlarmListActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm_list);
+
+        ListView alarmView = (ListView) findViewById(R.id.alarmView);
+
     }
 
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
